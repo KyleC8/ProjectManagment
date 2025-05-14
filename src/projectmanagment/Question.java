dc60fd689b2235df9011bb7cbb79c77e9536693f
KyleC8 <kycla2624@ugcloud.ca>
2025-05-14 11:11:22 -0400

    Add files via upload

---
+++ src/projectmanagment/Question.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication3;
+
+public class Question {
+    private String question;
+    private String answer;
+    
+    public Question(String question, String answer){
+        this.question = question;
+        this.answer = answer;
+    }
+    
+    public String getQuestion(){
+        return question;
+    }
+    
+    public String getAnswer(){
+        return answer;
+    }
+    
+    public boolean isGuessed(String guess){
+        return guess.equals(answer);
+    }
+    
+    public boolean equals(Question q){
+        return q.answer.equals(answer) && q.question.equals(question);
+    }
+    
+    public Question clone(){
+        return new Question(question, answer);
+    }
+    
+    public String toString(){
+        return "Question: " + question + "\nAnswer: " + answer;
+    }
+}
