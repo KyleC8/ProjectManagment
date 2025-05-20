21756bb6657c2015d3d777b38d49f989012e4314
KyleC8 <kycla2624@ugcloud.ca>
2025-05-19 22:57:17 -0400

    Update Quiz.java

--- src/projectmanagment/Quiz.java
+++ src/projectmanagment/Quiz.java
@@ -1,44 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package projectmanagment;
+
 import javax.swing.JOptionPane;
 import java.text.DecimalFormat;
 
 public class Quiz {
-    private Question questions[];
-    
-    public Quiz(Question questions[]){
+    private Question[] questions;
+
+    public Quiz(Question[] questions) {
         this.questions = questions;
     }
-    
-    public void doQuiz(){
+
+    public void doQuiz() {
         DecimalFormat percent = new DecimalFormat("#,##0%");
         String answer;
         int length = questions.length;
         int numCorrect = 0;
-        for(int i = 0; i < length; i ++){
-            answer = JOptionPane.showInputDialog(null, questions[i]);
-            if(questions[i].isGuessed(answer)){
-                numCorrect ++;
-                JOptionPane.showMessageDialog(null, "You got it correct!!! " 
-                        + (length - i) + " question left");
-            } else{
-                JOptionPane.showMessageDialog(null, 
-                        "You got it wrong :( the correct answer was " + questions[i].getAnswer()
-                        + (length - i) + " question left");
+
+        for (int i = 0; i < length; i++) {
+            answer = JOptionPane.showInputDialog(null, questions[i].getQuestion());
+            if (questions[i].isGuessed(answer)) {
+                numCorrect++;
+                JOptionPane.showMessageDialog(null,
+                        "You got it correct! " + (length - i - 1) + " question(s) left.");
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "You got it wrong :( The correct answer was: " + questions[i].getAnswer()
+                        + ". " + (length - i - 1) + " question(s) left.");
             }
         }
-        JOptionPane.showMessageDialog(null, "You got " + numCorrect
-                + " out of " + length + " (" + percent.format(numCorrect / length) + ")");
+
+        double score = (double) numCorrect / length;
+        JOptionPane.showMessageDialog(null,
+                "You got " + numCorrect + " out of " + length + " (" + percent.format(score) + ")");
     }
-    
-    public String toString(){
+
+    public String toString() {
         String output = "Quiz";
-        for(int i = 0; i < questions.length; i ++){
+        for (int i = 0; i < questions.length; i++) {
             output += "\n" + questions[i].toString();
         }
         return output;
-    }
+    }   
 }
